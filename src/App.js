7714f4d659a503355db9159fff01be45fdfc4571
Tunahan Tatli <tunahantatl@gmail.com>
2022-10-09 21:53:59 +0300

    cmmt-add-navbar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-     
+      <div id="navbar">
+        <ul>
+          <li><a className="btn"href="#">ABOUT US</a></li>
+          <li><a className="btn"href="#">FOR YOU</a></li>
+          <li><a className="btn"href="#">SERVICES</a></li>
+          <li><a className="btn"href="#">BLOG</a></li>
+          <li><a className="btn"href="#">VLOG</a></li>
+          <li><a className="btn"href="#">CONTACT</a></li>
+        </ul>
+      </div>
     </div>
   );
 }
